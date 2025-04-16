fb4e35e2df7fe53f315738d35281731c93b6ab83
DeadEgos <40hzshadow@gmail.com>
2025-04-16 11:42:11 -0700

    wip: add params for streams followed

--- lib/teac/api/streams.ex
+++ lib/teac/api/streams.ex
@@ -19,13 +19,14 @@ defmodule Teac.Api.Streams do
     def get(opts) do
       token = Keyword.fetch!(opts, :token)
       client_id = Keyword.fetch!(opts, :client_id)
+      user_id = Keyword.fetch!(opts, :user_id)
 
       case Req.get!(Teac.Api.api_uri() <> "streams/followed",
              headers: [
                {"Authorization", "Bearer #{token}"},
                {"Client-Id", client_id}
              ],
-             params: []
+             params: [user_id: user_id]
            ) do
         %Req.Response{status: 200, body: %{"data" => data}} -> {:ok, data}
         %Req.Response{body: body} -> {:error, body}
