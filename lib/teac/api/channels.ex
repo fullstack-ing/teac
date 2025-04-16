de76c7266d7a1a1af5daaa5339239b6e92d89273
DeadEgos <40hzshadow@gmail.com>
2025-04-16 10:15:56 -0700

    feat: add followers params to request

--- lib/teac/api/channels.ex
+++ lib/teac/api/channels.ex
@@ -132,13 +132,21 @@ defmodule Teac.Api.Channels do
     def get(opts) do
       token = Keyword.fetch!(opts, :token)
       client_id = Keyword.fetch!(opts, :client_id)
+      broadcaster_id = Keyword.fetch!(opts, :broadcaster_id)
+      params = [broadcaster_id: broadcaster_id]
+
+      params =
+        case Keyword.get(opts, :user_id) do
+          nil -> params
+          user_id -> params ++ [user_id: user_id]
+        end
 
       case Req.get!(Teac.Api.api_uri() <> "channels/followers",
              headers: [
                {"Authorization", "Bearer #{token}"},
                {"Client-Id", client_id}
              ],
-             params: []
+             params: params
            ) do
         %Req.Response{status: 200, body: %{"data" => data}} -> {:ok, data}
         %Req.Response{body: body} -> {:error, body}
