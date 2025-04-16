8655854565d0efa4c48899935f1185cf9a54dbac
DeadEgos <40hzshadow@gmail.com>
2025-04-16 09:30:02 -0700

    wip: added params to channel point's get redemptions

--- lib/teac/api/channel_points.ex
+++ lib/teac/api/channel_points.ex
@@ -91,15 +91,47 @@ defmodule Teac.Api.ChannelPoints do
       token = Keyword.fetch!(opts, :token)
       client_id = Keyword.fetch!(opts, :client_id)
 
-      case Req.get!(Teac.Api.api_uri() <> "channel_points/custom_rewards/redemptions",
-             headers: [
-               {"Authorization", "Bearer #{token}"},
-               {"Client-Id", client_id}
-             ],
-             params: []
-           ) do
-        %Req.Response{status: 200, body: %{"data" => data}} -> {:ok, data}
-        %Req.Response{body: body} -> {:error, body}
+      params =
+        case Keyword.get(opts, :broadcaster_id) do
+          nil -> []
+          broadcaster_id -> [broadcaster_id: broadcaster_id]
+        end
+
+      params =
+        case Keyword.get(opts, :status) do
+          nil -> params
+          status -> params ++ [status: status]
+        end
+
+      params =
+        case Keyword.get(opts, :reward_id) do
+          nil -> params
+          reward_id -> params ++ [reward_id: reward_id]
+        end
+
+      ids = opts |> Keyword.get(:ids, []) |> List.wrap() |> Enum.map(&to_string/1)
+      total = length(ids)
+
+      cond do
+        total > 100 ->
+          {:error, "Total IDs and or Names and or IGDB IDs cannot exceed 100"}
+
+        true ->
+          # Convert to repeated query params
+          params =
+            (Enum.map(ids, &{:id, &1}) ++ params)
+            |> dbg()
+
+          case Req.get!(Teac.Api.api_uri() <> "channel_points/custom_rewards/redemptions",
+                 headers: [
+                   {"Authorization", "Bearer #{token}"},
+                   {"Client-Id", client_id}
+                 ],
+                 params: params
+               ) do
+            %Req.Response{status: 200, body: %{"data" => data}} -> {:ok, data}
+            %Req.Response{body: body} -> {:error, body}
+          end
       end
     end
 
