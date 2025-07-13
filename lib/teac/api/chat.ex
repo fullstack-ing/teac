d231eaecfdca7d6a34586609781b945a17cb9715
FullStack.ing <186414880+fullstack-ing@users.noreply.github.com>
2025-07-13 11:49:26 -0700

    fix: add user_id to chat color endpoint

--- lib/teac/api/chat.ex
+++ lib/teac/api/chat.ex
@@ -80,12 +80,14 @@ defmodule Teac.Api.Chat do
     def get(opts) do
       token = Keyword.fetch!(opts, :token)
       client_id = Keyword.get(opts, :client_id, Teac.client_id())
+      user_id = Keyword.fetch!(opts, :user_id)
 
       case Req.get!(Teac.api_uri() <> "chat/badges/global",
              headers: [
                {"Authorization", "Bearer #{token}"},
                {"Client-Id", client_id}
-             ]
+             ],
+             params: [user_id: user_id]
            ) do
         %Req.Response{status: 200, body: %{"data" => data}} -> {:ok, data}
         %Req.Response{body: body} -> {:error, body}
@@ -115,13 +117,14 @@ defmodule Teac.Api.Chat do
     def get(opts) do
       token = Keyword.fetch!(opts, :token)
       client_id = Keyword.get(opts, :client_id, Teac.client_id())
+      user_id = Keyword.fetch!(opts, :user_id)
 
       case Req.get!(Teac.api_uri() <> "chat/color",
              headers: [
                {"Authorization", "Bearer #{token}"},
                {"Client-Id", client_id}
              ],
-             params: []
+             params: [user_id: user_id]
            ) do
         %Req.Response{status: 200, body: %{"data" => data}} -> {:ok, data}
         %Req.Response{body: body} -> {:error, body}
