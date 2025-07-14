9b87d3bad6468e5d02e0da2f2884d153ab0f4ca2
FullStack.ing <186414880+fullstack-ing@users.noreply.github.com>
2025-07-13 17:09:16 -0700

    fix: clips args

--- lib/teac/api/clips.ex
+++ lib/teac/api/clips.ex
@@ -2,13 +2,14 @@ defmodule Teac.Api.Clips do
   def get(opts) do
     token = Keyword.fetch!(opts, :token)
     client_id = Keyword.get(opts, :client_id, Teac.client_id())
+    broadcaster_id = Keyword.fetch!(opts, :broadcaster_id)
 
     case Req.get!(Teac.api_uri() <> "clips",
            headers: [
              {"Authorization", "Bearer #{token}"},
              {"Client-Id", client_id}
            ],
-           params: []
+           params: [broadcaster_id: broadcaster_id]
          ) do
       %Req.Response{status: 200, body: %{"data" => data}} -> {:ok, data}
       %Req.Response{body: body} -> {:error, body}
