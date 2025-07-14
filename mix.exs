d9f11880277906eb326edcb00ef6205954eab41f
FullStack.ing <186414880+fullstack-ing@users.noreply.github.com>
2025-07-13 17:53:54 -0700

    feat: gen docs

--- mix.exs
+++ mix.exs
@@ -4,10 +4,17 @@ defmodule Teac.MixProject do
   def project do
     [
       app: :teac,
+      name: "teac",
       version: "0.1.0",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package(),
+      source_url: "https://github.com/fullstack-ing/teac",
+      homepage_url: "https://github.com/fullstack-ing/teac",
+      description: description(),
+      docs: docs(),
+      package: package()
     ]
   end
 
@@ -22,9 +29,34 @@ defmodule Teac.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:req, "~> 0.5.10"}
+      {:req, "~> 0.5.10"},
+      {:ex_doc, "~> 0.28", only: :dev, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp description do
+    """
+    An Elixir HTTP/REST and Websocket Client for the Twitch API.
+    """
+  end
+
+  defp docs do
+    [
+      main: "readme",
+      # logo: "path/to/logo.png",
+      extras: ["README.md"]
+    ]
+  end
+
+  defp package do
+    [
+      licenses: ["Apache-2.0"],
+      links: %{
+        "GitHub" => "https://github.com/fullstack-ing/teac"
+      },
+      description: "Twitch Elixir API Client"
     ]
   end
 end
