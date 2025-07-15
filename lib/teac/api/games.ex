505ccfe2fae4433b08be1b512f6400cab881f9c4
FullStack.ing <186414880+fullstack-ing@users.noreply.github.com>
2025-07-15 10:02:09 -0700

    feat: compelete Games.get/1 closes #64

--- lib/teac/api/games.ex
+++ lib/teac/api/games.ex
@@ -1,35 +1,91 @@
 defmodule Teac.Api.Games do
+  alias Teac.Api
+  import Ecto.Changeset
+
   def get(opts) do
-    token = Keyword.fetch!(opts, :token)
-    client_id = Keyword.get(opts, :client_id, Teac.client_id())
+    case validate_get_opts(opts) do
+      {:ok, %{token: token, client_id: client_id} = cs} ->
+        ids =
+          cs
+          |> Map.get(:ids, [])
+          |> Enum.map(&{:id, &1})
 
-    ids = opts |> Keyword.get(:ids, []) |> List.wrap() |> Enum.map(&to_string/1)
-    names = opts |> Keyword.get(:names, []) |> List.wrap() |> Enum.map(&to_string/1)
-    igdb_id = opts |> Keyword.get(:igdb_ids, []) |> List.wrap() |> Enum.map(&to_string/1)
+        names =
+          cs
+          |> Map.get(:names, [])
+          |> Enum.map(&{:name, &1})
 
-    total = length(ids) + length(names) + length(igdb_id)
+        igdb_ids =
+          cs
+          |> Map.get(:igdb_ids, [])
+          |> Enum.map(&{:igdb_id, &1})
 
-    cond do
-      total > 100 ->
-        {:error, "Total IDs and or Names and or IGDB IDs cannot exceed 100"}
+        [
+          base_url: Api.uri("games"),
+          params: ids ++ names ++ igdb_ids,
+          headers: Api.headers(token, client_id)
+        ]
+        |> Keyword.merge(Application.get_env(:teac, :api_req_options, []))
+        |> Req.get!()
+        |> Api.handle_response()
 
-      true ->
-        # Convert to repeated query params
-        params =
-          Enum.map(ids, &{:id, &1}) ++
-            Enum.map(names, &{:name, &1}) ++
-            Enum.map(igdb_id, &{:igdb_id, &1})
+      error ->
+        error
+    end
+  end
 
-        case Req.get!(Teac.api_uri() <> "games",
-               headers: [
-                 {"Authorization", "Bearer #{token}"},
-                 {"Client-Id", client_id}
-               ],
-               params: params
-             ) do
-          %Req.Response{status: 200, body: %{"data" => data}} -> {:ok, data}
-          %Req.Response{body: body} -> {:error, body}
-        end
+  defp validate_get_opts(opt) do
+    data = %{}
+
+    types = %{
+      token: :string,
+      client_id: :string,
+      ids: {:array, :string},
+      names: {:array, :string},
+      igdb_ids: {:array, :string}
+    }
+
+    changeset =
+      {data, types}
+      |> cast(opt |> Map.new(), Map.keys(types))
+      |> Api.default_client_id()
+      |> validate_required([:token, :client_id])
+      |> validate_includes_at_least_one_param()
+      |> validate_limit_on_params()
+
+    case apply_action(changeset, :insert) do
+      {:ok, data} -> {:ok, data}
+      {:error, %Ecto.Changeset{errors: errors}} -> {:error, errors}
+    end
+  end
+
+  def validate_includes_at_least_one_param(%{changes: changes} = cs) do
+    case Map.get(changes, :ids) || Map.get(changes, :names) || Map.get(changes, :igdb_ids) do
+      nil ->
+        cs
+        |> add_error(
+          :params,
+          "Requires at least one of these params 'ids', 'names' and or 'igdb_ids'"
+        )
+
+      _ ->
+        cs
+    end
+  end
+
+  def validate_limit_on_params(%{changes: changes} = cs) do
+    ids = Map.get(changes, :ids, []) |> Enum.count()
+    names = Map.get(changes, :names, []) |> Enum.count()
+    igdbs = Map.get(changes, :igdb_ids, []) |> Enum.count()
+
+    if ids + names + igdbs > 100 do
+      cs
+      |> add_error(
+        :params,
+        "Exceeds more than 100 'ids', 'names' and or 'igdb_ids'"
+      )
+    else
+      cs
     end
   end
 end
--- test/teac/api/games_test.exs
+++ test/teac/api/games_test.exs
@@ -1,4 +1,5 @@
 defmodule Teac.Api.GamesTest do
+  alias Teac.Api.Games
   use ExUnit.Case, async: true
 
   test "get/1 valid" do
@@ -6,7 +7,39 @@ defmodule Teac.Api.GamesTest do
       Req.Test.json(conn, %{"data" => %{}})
     end)
 
-    # fixme
-    assert false
+    assert Games.get(ids: ["1"], token: "token") == {:ok, %{}}
+    assert Games.get(names: ["foo", "bar"], token: "token") == {:ok, %{}}
+    assert Games.get(igdb_ids: ["1", "2", "3"], token: "token", client_id: "asdf") == {:ok, %{}}
+
+    assert Games.get(
+             ids: 1..33 |> Enum.map(&to_string/1),
+             names: 1..33 |> Enum.map(&to_string/1),
+             igdb_ids: 1..33 |> Enum.map(&to_string/1),
+             token: "token"
+           ) == {:ok, %{}}
+  end
+
+  test "get/2 invalid" do
+    Req.Test.stub(Teac.Api, fn conn ->
+      Req.Test.json(conn, %{"data" => %{}})
+    end)
+
+    assert Games.get(token: "token") ==
+             {:error,
+              [
+                params:
+                  {"Requires at least one of these params 'ids', 'names' and or 'igdb_ids'", []}
+              ]}
+
+    assert Games.get(ids: 1..101 |> Enum.map(&to_string/1), token: "token") ==
+             {:error, [params: {"Exceeds more than 100 'ids', 'names' and or 'igdb_ids'", []}]}
+
+    assert Games.get(
+             ids: 1..33 |> Enum.map(&to_string/1),
+             names: 1..33 |> Enum.map(&to_string/1),
+             igdb_ids: 1..36 |> Enum.map(&to_string/1),
+             token: "token"
+           ) ==
+             {:error, [params: {"Exceeds more than 100 'ids', 'names' and or 'igdb_ids'", []}]}
   end
 end
