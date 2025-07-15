dd5538eca5b45feba4056327d8d6d9b89dbe54a5
FullStack.ing <186414880+fullstack-ing@users.noreply.github.com>
2025-07-15 15:02:31 -0700

    feat: complete teams.channel, closes #125

--- test/teac/api/teams/channel_test.exs
+++ test/teac/api/teams/channel_test.exs
@@ -1,12 +1,28 @@
 defmodule Teac.Api.Teams.ChannelTest do
   use ExUnit.Case, async: true
+  alias Teac.Api.Teams.Channel
 
   test "get/1 valid" do
     Req.Test.stub(Teac.Api, fn conn ->
       Req.Test.json(conn, %{"data" => %{}})
     end)
 
-    # fixme
-    assert false
+    assert Channel.get(broadcaster_id: "1", token: "token") == {:ok, %{}}
+    assert Channel.get(broadcaster_id: "1", token: "token", client_id: "asdf") == {:ok, %{}}
+  end
+
+  test "get/1 invalid" do
+    Req.Test.stub(Teac.Api, fn conn ->
+      Req.Test.json(conn, %{"data" => %{}})
+    end)
+
+    assert Channel.get(token: "asdf") ==
+             {:error, [broadcaster_id: {"can't be blank", [validation: :required]}]}
+
+    assert Channel.get(broadcaster_id: 1, token: "asdf") ==
+             {:error, [broadcaster_id: {"is invalid", [type: :string, validation: :cast]}]}
+
+    assert Channel.get(broadcaster_id: "1") ==
+             {:error, [token: {"can't be blank", [validation: :required]}]}
   end
 end
