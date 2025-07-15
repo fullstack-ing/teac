a82d1b24de4e07ad87d55daf7097ceb27e837702
FullStack.ing <186414880+fullstack-ing@users.noreply.github.com>
2025-07-15 10:46:05 -0700

    feat: complete ContentClassificationLabels.get/1, closes #42

--- lib/teac/api/content_classification_labels.ex
+++ lib/teac/api/content_classification_labels.ex
@@ -1,17 +1,85 @@
 defmodule Teac.Api.ContentClassificationLabels do
-  def get(opts) do
-    token = Keyword.fetch!(opts, :token)
-    client_id = Keyword.get(opts, :client_id, Teac.client_id())
+  alias Teac.Api
+  import Ecto.Changeset
 
-    case Req.get!(Teac.api_uri() <> "content_classification_labels",
-           headers: [
-             {"Authorization", "Bearer #{token}"},
-             {"Client-Id", client_id}
-           ],
-           params: []
-         ) do
-      %Req.Response{status: 200, body: %{"data" => data}} -> {:ok, data}
-      %Req.Response{body: body} -> {:error, body}
+  @locales [
+    "en-US",
+    "bg-BG",
+    "cs-CZ",
+    "da-DK",
+    "da-DK",
+    "de-DE",
+    "el-GR",
+    "en-GB",
+    "en-US",
+    "es-ES",
+    "es-MX",
+    "fi-FI",
+    "fr-FR",
+    "hu-HU",
+    "it-IT",
+    "ja-JP",
+    "ko-KR",
+    "nl-NL",
+    "no-NO",
+    "pl-PL",
+    "pt-BT",
+    "pt-PT",
+    "ro-RO",
+    "ru-RU",
+    "sk-SK",
+    "sv-SE",
+    "th-TH",
+    "tr-TR",
+    "vi-VN",
+    "zh-CN",
+    "zh-TW"
+    ###########
+  ]
+
+  def get(opts) do
+    case validate_get_opts(opts) do
+      {:ok, %{token: token, client_id: client_id} = cs} ->
+        params =
+          if Map.get(cs, :locale, nil) do
+            [{:locale, Map.get(cs, :locale, nil)}]
+          else
+            []
+          end
+
+        [
+          base_url: Api.uri("content_classification_labels"),
+          params: params,
+          headers: Api.headers(token, client_id)
+        ]
+        |> Keyword.merge(Application.get_env(:teac, :api_req_options, []))
+        |> Req.get!()
+        |> Api.handle_response()
+
+      error ->
+        error
+    end
+  end
+
+  defp validate_get_opts(opt) do
+    data = %{}
+
+    types = %{
+      token: :string,
+      client_id: :string,
+      locale: :string
+    }
+
+    changeset =
+      {data, types}
+      |> cast(opt |> Map.new(), Map.keys(types))
+      |> Api.default_client_id()
+      |> validate_required([:token, :client_id])
+      |> validate_inclusion(:locale, @locales)
+
+    case apply_action(changeset, :insert) do
+      {:ok, data} -> {:ok, data}
+      {:error, %Ecto.Changeset{errors: errors}} -> {:error, errors}
     end
   end
 end
--- test/teac/api/content_classification_labels_test.exs
+++ test/teac/api/content_classification_labels_test.exs
@@ -1,4 +1,5 @@
 defmodule Teac.Api.ContentClassificationLabelsTest do
+  alias Teac.Api.ContentClassificationLabels
   use ExUnit.Case, async: true
 
   test "get/1 valid" do
@@ -6,7 +7,61 @@ defmodule Teac.Api.ContentClassificationLabelsTest do
       Req.Test.json(conn, %{"data" => %{}})
     end)
 
-    # fixme
-    assert false
+    assert ContentClassificationLabels.get(token: "token") == {:ok, %{}}
+    assert ContentClassificationLabels.get(token: "token", client_id: "client_id") == {:ok, %{}}
+    assert ContentClassificationLabels.get(locale: "en-US", token: "token") == {:ok, %{}}
+  end
+
+  test "get/1 invalid" do
+    Req.Test.stub(Teac.Api, fn conn ->
+      Req.Test.json(conn, %{"data" => %{}})
+    end)
+
+    assert ContentClassificationLabels.get([]) ==
+             {:error, [token: {"can't be blank", [validation: :required]}]}
+
+    assert ContentClassificationLabels.get(locale: "foo", token: "token") == {
+             :error,
+             [
+               locale:
+                 {"is invalid",
+                  [
+                    validation: :inclusion,
+                    enum: [
+                      "en-US",
+                      "bg-BG",
+                      "cs-CZ",
+                      "da-DK",
+                      "da-DK",
+                      "de-DE",
+                      "el-GR",
+                      "en-GB",
+                      "en-US",
+                      "es-ES",
+                      "es-MX",
+                      "fi-FI",
+                      "fr-FR",
+                      "hu-HU",
+                      "it-IT",
+                      "ja-JP",
+                      "ko-KR",
+                      "nl-NL",
+                      "no-NO",
+                      "pl-PL",
+                      "pt-BT",
+                      "pt-PT",
+                      "ro-RO",
+                      "ru-RU",
+                      "sk-SK",
+                      "sv-SE",
+                      "th-TH",
+                      "tr-TR",
+                      "vi-VN",
+                      "zh-CN",
+                      "zh-TW"
+                    ]
+                  ]}
+             ]
+           }
   end
 end
