5029ce02d2ab29a1b21b89ba8b53696e724aef76
FullStack.ing <186414880+fullstack-ing@users.noreply.github.com>
2025-07-15 12:17:46 -0700

    feat: completed chat.color.get/put closes #30 and #31

--- lib/teac/api/chat/color.ex
+++ lib/teac/api/chat/color.ex
@@ -1,36 +1,144 @@
 defmodule Teac.Api.Chat.Color do
-  def get(opts) do
-    token = Keyword.fetch!(opts, :token)
-    client_id = Keyword.get(opts, :client_id, Teac.client_id())
-    user_id = Keyword.fetch!(opts, :user_id)
+  alias Teac.Api
+  import Ecto.Changeset
 
-    case Req.get!(Teac.api_uri() <> "chat/color",
-           headers: [
-             {"Authorization", "Bearer #{token}"},
-             {"Client-Id", client_id}
-           ],
-           params: [user_id: user_id]
-         ) do
-      %Req.Response{status: 200, body: %{"data" => data}} -> {:ok, data}
-      %Req.Response{body: body} -> {:error, body}
+  @color_labels [
+    "blue",
+    "blue_violet",
+    "cadet_blue",
+    "chocolate",
+    "coral",
+    "dodger_blue",
+    "firebrick",
+    "golden_rod",
+    "green",
+    "hot_pink",
+    "orange_red",
+    "red",
+    "sea_green",
+    "spring_green",
+    "yellow_green"
+  ]
+
+  @doc """
+  Gets the color used for the user’s name in chat.
+
+  ## Authorization
+  Requires an app access token or user access token.
+
+  ## Opts params
+  * token: String - required
+  * client_id: String - optional (defaults)
+  * user_ids: List of string - required
+  Max 100 ids
+
+  """
+  def get(opts) do
+    case validate_get_opts(opts) do
+      {:ok, %{token: token, client_id: client_id, user_id: user_id}} ->
+        [
+          base_url: Api.uri("chat/color"),
+          params: [user_id: user_id],
+          headers: Api.headers(token, client_id)
+        ]
+        |> Keyword.merge(Application.get_env(:teac, :api_req_options, []))
+        |> Req.get!()
+        |> Api.handle_response()
+
+      error ->
+        error
+    end
+  end
+
+  defp validate_get_opts(opt) do
+    data = %{}
+
+    types = %{
+      token: :string,
+      client_id: :string,
+      user_id: :string
+    }
+
+    changeset =
+      {data, types}
+      |> cast(opt |> Map.new(), Map.keys(types))
+      |> Api.default_client_id()
+      |> validate_required([:token, :client_id, :user_id])
+
+    case apply_action(changeset, :insert) do
+      {:ok, data} -> {:ok, data}
+      {:error, %Ecto.Changeset{errors: errors}} -> {:error, errors}
     end
   end
 
   def put(opts) do
-    token = Keyword.fetch!(opts, :token)
-    client_id = Keyword.get(opts, :client_id, Teac.client_id())
+    case validate_put_opts(opts) do
+      {:ok, %{token: token, client_id: client_id, user_id: user_id} = cs} ->
+        color_hex =
+          if Map.get(cs, :color_hex, nil) do
+            [{:color, Map.get(cs, :color_hex, nil)}]
+          else
+            []
+          end
 
-    case Req.put!(Teac.api_uri() <> "chat/color",
-           headers: [
-             {"Authorization", "Bearer #{token}"},
-             {"Client-Id", client_id},
-             {"Content-Type", "application/x-www-form-urlencoded"}
-           ],
-           form: [],
-           decode_body: :json
-         ) do
-      %Req.Response{status: 200, body: %{"data" => data}} -> {:ok, data}
-      %Req.Response{body: body} -> {:error, body}
+        color_label =
+          if Map.get(cs, :color_label, nil) do
+            [{:color, Map.get(cs, :color_label, nil)}]
+          else
+            []
+          end
+
+        [
+          base_url: Api.uri("chat/color"),
+          params: [user_id: user_id] ++ color_hex ++ color_label,
+          headers: Api.headers(token, client_id)
+        ]
+        |> Keyword.merge(Application.get_env(:teac, :api_req_options, []))
+        |> Req.put!()
+        |> Api.handle_response()
+
+      error ->
+        error
+    end
+  end
+
+  defp validate_put_opts(opt) do
+    data = %{}
+
+    types = %{
+      token: :string,
+      client_id: :string,
+      user_id: :string,
+      color_label: :string,
+      color_hex: :string
+    }
+
+    changeset =
+      {data, types}
+      |> cast(opt |> Map.new(), Map.keys(types))
+      |> Api.default_client_id()
+      |> validate_required([:token, :client_id, :user_id])
+      |> validate_includes_at_least_one_param()
+      |> validate_inclusion(:color_label, @color_labels)
+      |> validate_format(:color_hex, ~r/^#(?:[0-9a-f]{3}){1,2}$/i)
+
+    case apply_action(changeset, :insert) do
+      {:ok, data} -> {:ok, data}
+      {:error, %Ecto.Changeset{errors: errors}} -> {:error, errors}
+    end
+  end
+
+  def validate_includes_at_least_one_param(%{changes: changes} = cs) do
+    case Map.get(changes, :color_label) || Map.get(changes, :color_hex) do
+      nil ->
+        cs
+        |> add_error(
+          :params,
+          "Requires at least one of these params 'color_label' or 'color_hex'"
+        )
+
+      _ ->
+        cs
     end
   end
 end
--- test/teac/api/chat/color_test.exs
+++ test/teac/api/chat/color_test.exs
@@ -1,13 +1,27 @@
 defmodule Teac.Api.Chat.ColorTest do
   use ExUnit.Case, async: true
 
+  alias Teac.Api.Chat.Color
+
   test "get/1 valid" do
     Req.Test.stub(Teac.Api, fn conn ->
       Req.Test.json(conn, %{"data" => %{}})
     end)
 
-    # fixme
-    assert false
+    assert Color.get(user_id: "asdf", token: "token") == {:ok, %{}}
+    assert Color.get(user_id: "asdf", token: "token", client_id: "asdf") == {:ok, %{}}
+  end
+
+  test "get/1 invalid" do
+    Req.Test.stub(Teac.Api, fn conn ->
+      Req.Test.json(conn, %{"data" => %{}})
+    end)
+
+    assert Color.get(user_id: "asdf") ==
+             {:error, [token: {"can't be blank", [validation: :required]}]}
+
+    assert Color.get(token: "token") ==
+             {:error, [user_id: {"can't be blank", [validation: :required]}]}
   end
 
   test "put/1 valid" do
@@ -15,7 +29,61 @@ defmodule Teac.Api.Chat.ColorTest do
       Req.Test.json(conn, %{"data" => %{}})
     end)
 
-    # fixme
-    assert false
+    assert Color.put(color_label: "green", user_id: "asdf", token: "token") == {:ok, %{}}
+    assert Color.put(color_hex: "#AABBFF", user_id: "asdf", token: "token") == {:ok, %{}}
+
+    assert Color.put(color_hex: "#AABBFF", color_label: "green", user_id: "asdf", token: "token") ==
+             {:ok, %{}}
+  end
+
+  test "put/1 invalid" do
+    Req.Test.stub(Teac.Api, fn conn ->
+      Req.Test.json(conn, %{"data" => %{}})
+    end)
+
+    assert Color.put(color_label: "green", token: "token") ==
+             {:error, [user_id: {"can't be blank", [validation: :required]}]}
+
+    assert Color.put(color_label: "green", user_id: "asdf") ==
+             {:error, [token: {"can't be blank", [validation: :required]}]}
+
+    assert Color.put(user_id: "asdf", token: "token") ==
+             {:error,
+              [params: {"Requires at least one of these params 'color_label' or 'color_hex'", []}]}
+
+    assert Color.put(color_hex: "green", user_id: "asdf") ==
+             {:error,
+              [
+                {:color_hex, {"has invalid format", [validation: :format]}},
+                {:token, {"can't be blank", [validation: :required]}}
+              ]}
+
+    assert Color.put(color_label: "foo", user_id: "asdf", token: "token") ==
+             {:error,
+              [
+                color_label:
+                  {"is invalid",
+                   [
+                     {:validation, :inclusion},
+                     {:enum,
+                      [
+                        "blue",
+                        "blue_violet",
+                        "cadet_blue",
+                        "chocolate",
+                        "coral",
+                        "dodger_blue",
+                        "firebrick",
+                        "golden_rod",
+                        "green",
+                        "hot_pink",
+                        "orange_red",
+                        "red",
+                        "sea_green",
+                        "spring_green",
+                        "yellow_green"
+                      ]}
+                   ]}
+              ]}
   end
 end
