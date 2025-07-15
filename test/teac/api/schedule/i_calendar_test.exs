5ba15146335a224a0a968d411971aa3ce68adc2e
FullStack.ing <186414880+fullstack-ing@users.noreply.github.com>
2025-07-15 14:52:23 -0700

    feat: complete ical, closes #109

--- test/teac/api/schedule/i_calendar_test.exs
+++ test/teac/api/schedule/i_calendar_test.exs
@@ -1,12 +1,28 @@
 defmodule Teac.Api.Schedule.ICalendarTest do
   use ExUnit.Case, async: true
+  alias Teac.Api.Schedule.ICalendar
 
   test "get/1 valid" do
     Req.Test.stub(Teac.Api, fn conn ->
       Req.Test.json(conn, %{"data" => %{}})
     end)
 
-    # fixme
-    assert false
+    assert ICalendar.get(broadcaster_id: "1", token: "token") == {:ok, %{}}
+    assert ICalendar.get(broadcaster_id: "1", token: "token", client_id: "asdf") == {:ok, %{}}
+  end
+
+  test "get/1 invalid" do
+    Req.Test.stub(Teac.Api, fn conn ->
+      Req.Test.json(conn, %{"data" => %{}})
+    end)
+
+    assert ICalendar.get(token: "asdf") ==
+             {:error, [broadcaster_id: {"can't be blank", [validation: :required]}]}
+
+    assert ICalendar.get(broadcaster_id: 1, token: "asdf") ==
+             {:error, [broadcaster_id: {"is invalid", [type: :string, validation: :cast]}]}
+
+    assert ICalendar.get(broadcaster_id: "1") ==
+             {:error, [token: {"can't be blank", [validation: :required]}]}
   end
 end
