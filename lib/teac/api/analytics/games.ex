06b49f6df00d563dfd8dbe42acec44a232f3ae9f
FullStack.ing <186414880+fullstack-ing@users.noreply.github.com>
2025-07-15 19:49:20 -0700

    feat: complete analytics.games.get, closes #3

--- lib/teac/api/analytics/games.ex
+++ lib/teac/api/analytics/games.ex
@@ -1,23 +1,60 @@
 defmodule Teac.Api.Analytics.Games do
-  @doc """
-  Gets an analytics report for one or more games. The response contains the URLs used to download the reports (CSV files). Learn more
+  alias Teac.Api
+  import Ecto.Changeset
 
-  ## Authorization
-  Requires a user access token that includes the analytics:read:games scope.
-  """
   def get(opts) do
-    token = Keyword.fetch!(opts, :token)
-    client_id = Keyword.get(opts, :client_id, Teac.client_id())
+    case validate_get_opts(opts) do
+      {:ok, %{token: token, client_id: client_id} = cs} ->
+        params =
+          [
+            game_id: Map.get(cs, :game_id, nil),
+            type: Map.get(cs, :type, nil),
+            started_at: Map.get(cs, :started_at, nil),
+            end_at: Map.get(cs, :end_at, nil),
+            first: Map.get(cs, :first, nil),
+            after: Map.get(cs, :after, nil)
+          ]
+          |> Enum.filter(fn {_, k} -> k != nil end)
 
-    case Req.get!(Teac.api_uri() <> "analytics/games",
-           headers: [
-             {"Authorization", "Bearer #{token}"},
-             {"Client-Id", client_id}
-           ],
-           params: []
-         ) do
-      %Req.Response{status: 200, body: %{"data" => data}} -> {:ok, data}
-      %Req.Response{body: body} -> {:error, body}
+        [
+          base_url: Api.uri("analytics/games"),
+          params: params,
+          headers: Api.headers(token, client_id)
+        ]
+        |> Keyword.merge(Application.get_env(:teac, :api_req_options, []))
+        |> Req.get!()
+        |> Api.handle_response()
+
+      error ->
+        error
+    end
+  end
+
+  defp validate_get_opts(opt) do
+    data = %{}
+
+    types = %{
+      token: :string,
+      client_id: :string,
+      type: :string,
+      game_id: :string,
+      started_at: :string,
+      end_at: :string,
+      first: :integer,
+      after: :string
+    }
+
+    changeset =
+      {data, types}
+      |> cast(opt |> Map.new(), Map.keys(types))
+      |> Api.default_client_id()
+      |> validate_required([:token, :client_id])
+      |> validate_inclusion(:type, ["overview_v2"])
+      |> validate_number(:first, greater_than: 0, less_than: 101)
+
+    case apply_action(changeset, :insert) do
+      {:ok, data} -> {:ok, data}
+      {:error, %Ecto.Changeset{errors: errors}} -> {:error, errors}
     end
   end
 end
--- test/teac/api/analytics/game_test.exs
+++ test/teac/api/analytics/game_test.exs
@@ -1,12 +1,44 @@
-defmodule Teac.Api.Analytics.GameTest do
+defmodule Teac.Api.Analytics.GamesTest do
   use ExUnit.Case, async: true
+  alias Teac.Api.Analytics.Games
 
   test "get/1 valid" do
     Req.Test.stub(Teac.Api, fn conn ->
       Req.Test.json(conn, %{"data" => %{}})
     end)
 
-    # fixme
-    assert false
+    assert Games.get(token: "token") == {:ok, %{}}
+    assert Games.get(token: "token", client_id: "asdf") == {:ok, %{}}
+    assert Games.get(token: "token", type: "overview_v2") == {:ok, %{}}
+    assert Games.get(token: "token", game_id: "1") == {:ok, %{}}
+    assert Games.get(token: "token", ended_at: "2021-10-27T00:00:00Z") == {:ok, %{}}
+    assert Games.get(token: "token", started_at: "2021-10-27T00:00:00Z") == {:ok, %{}}
+    assert Games.get(token: "token", first: 100) == {:ok, %{}}
+    assert Games.get(token: "token", first: 1) == {:ok, %{}}
+  end
+
+  test "get/1 invalid" do
+    Req.Test.stub(Teac.Api, fn conn ->
+      Req.Test.json(conn, %{"data" => %{}})
+    end)
+
+    assert Games.get([]) ==
+             {:error, [token: {"can't be blank", [validation: :required]}]}
+
+    assert Games.get(token: "token", first: 101) ==
+             {:error,
+              [
+                first:
+                  {"must be less than %{number}",
+                   [validation: :number, kind: :less_than, number: 101]}
+              ]}
+
+    assert Games.get(token: "token", first: 0) ==
+             {:error,
+              [
+                first:
+                  {"must be greater than %{number}",
+                   [validation: :number, kind: :greater_than, number: 0]}
+              ]}
   end
 end
