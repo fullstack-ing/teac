1ef61e67f8b95163984c1d49fa14bdaabd2e00c4
FullStack.ing <186414880+fullstack-ing@users.noreply.github.com>
2025-07-16 09:43:42 -0700

    feat: complete videos.get, closes #134

--- lib/teac/api/videos.ex
+++ lib/teac/api/videos.ex
@@ -1,18 +1,71 @@
 defmodule Teac.Api.Videos do
-  def get(opts) do
-    token = Keyword.fetch!(opts, :token)
-    client_id = Keyword.get(opts, :client_id, Teac.client_id())
-    user_id = Keyword.fetch!(opts, :user_id)
+  alias Teac.Api
+  import Ecto.Changeset
 
-    case Req.get!(Teac.api_uri() <> "videos",
-           headers: [
-             {"Authorization", "Bearer #{token}"},
-             {"Client-Id", client_id}
-           ],
-           params: [user_id: user_id]
-         ) do
-      %Req.Response{status: 200, body: %{"data" => data}} -> {:ok, data}
-      %Req.Response{body: body} -> {:error, body}
+  def get(opts) do
+    case validate_get_opts(opts) do
+      {:ok,
+       %{token: token, client_id: client_id, ids: ids, user_id: user_id, game_id: game_id} = args} ->
+        params =
+          ids
+          |> Enum.map(&to_string/1)
+          |> Enum.map(&{:id, &1})
+          |> Keyword.merge(
+            user_id: user_id,
+            game_id: game_id,
+            language: Map.get(args, :language, nil),
+            period: Map.get(args, :period, nil),
+            sort: Map.get(args, :sort, nil),
+            type: Map.get(args, :v, nil),
+            first: Map.get(args, :first, nil),
+            after: Map.get(args, :after, nil),
+            before: Map.get(args, :before, nil)
+          )
+          |> Keyword.filter(fn {_, v} -> v != nil end)
+
+        [
+          base_url: Api.uri("videos"),
+          params: params,
+          headers: Api.headers(token, client_id)
+        ]
+        |> Keyword.merge(Application.get_env(:teac, :api_req_options, []))
+        |> Req.get!()
+        |> Api.handle_response()
+
+      error ->
+        error
+    end
+  end
+
+  defp validate_get_opts(opt) do
+    types = %{
+      ids: {:array, :string},
+      user_id: :string,
+      game_id: :string,
+      language: :string,
+      period: :string,
+      sort: :string,
+      type: :string,
+      first: :integer,
+      after: :string,
+      before: :string,
+      token: :string,
+      client_id: :string
+    }
+
+    changeset =
+      {%{}, types}
+      |> cast(Map.new(opt), Map.keys(types))
+      |> Api.default_client_id()
+      |> validate_inclusion(:period, ["all", "day", "week", "month"])
+      |> validate_inclusion(:sort, ["time", "trending", "views"])
+      |> validate_inclusion(:type, ["all", "archive", "highlight", "upload"])
+      |> validate_number(:first, greater_than: 0, less_than: 101)
+      |> validate_required([:token, :client_id, :ids, :user_id, :game_id])
+
+    case apply_action(changeset, :insert) do
+      {:ok, data} -> {:ok, data}
+      {:error, %Ecto.Changeset{errors: errors}} -> {:error, errors}
     end
   end
 
--- test/teac/api/videos_test.exs
+++ test/teac/api/videos_test.exs
@@ -1,13 +1,103 @@
 defmodule Teac.Api.VideosTest do
   use ExUnit.Case, async: true
+  alias Teac.Api.Videos
+
+  @args [
+    token: "token",
+    client_id: "asdf",
+    ids: ["asdf"],
+    user_id: "user_id",
+    game_id: "game_id",
+    language: "language",
+    period: "all",
+    sort: "time",
+    type: "all",
+    first: 1,
+    after: "1",
+    before: "1"
+  ]
 
   test "get/1 valid" do
     Req.Test.stub(Teac.Api, fn conn ->
       Req.Test.json(conn, %{"data" => %{}})
     end)
 
-    # fixme
-    assert false
+    assert Videos.get(@args) == {:ok, %{}}
+    assert Videos.get(@args |> Keyword.delete(:client_id)) == {:ok, %{}}
+  end
+
+  test "get/1 invalid" do
+    Req.Test.stub(Teac.Api, fn conn ->
+      Req.Test.json(conn, %{"data" => %{}})
+    end)
+
+    assert Videos.get(@args |> Keyword.delete(:token)) ==
+             {:error, [token: {"can't be blank", [validation: :required]}]}
+
+    assert Videos.get(@args |> Keyword.delete(:ids)) ==
+             {:error, [ids: {"can't be blank", [validation: :required]}]}
+
+    assert Videos.get(@args |> Keyword.delete(:user_id)) ==
+             {:error, [user_id: {"can't be blank", [validation: :required]}]}
+
+    assert Videos.get(@args |> Keyword.delete(:game_id)) ==
+             {:error, [game_id: {"can't be blank", [validation: :required]}]}
+
+    assert Videos.get(
+             ids: ["asdf"],
+             user_id: "asdf",
+             game_id: "asdf",
+             token: "asdf",
+             period: "foo"
+           ) ==
+             {:error,
+              [
+                period:
+                  {"is invalid",
+                   [{:validation, :inclusion}, {:enum, ["all", "day", "week", "month"]}]}
+              ]}
+
+    assert Videos.get(
+             ids: ["asdf"],
+             user_id: "asdf",
+             game_id: "asdf",
+             token: "asdf",
+             type: "foo"
+           ) ==
+             {:error,
+              [
+                type:
+                  {"is invalid",
+                   [validation: :inclusion, enum: ["all", "archive", "highlight", "upload"]]}
+              ]}
+
+    assert Videos.get(
+             ids: ["asdf"],
+             user_id: "asdf",
+             game_id: "asdf",
+             token: "asdf",
+             first: 0
+           ) ==
+             {:error,
+              [
+                first:
+                  {"must be greater than %{number}",
+                   [validation: :number, kind: :greater_than, number: 0]}
+              ]}
+
+    assert Videos.get(
+             ids: ["asdf"],
+             user_id: "asdf",
+             game_id: "asdf",
+             token: "asdf",
+             first: 101
+           ) ==
+             {:error,
+              [
+                first:
+                  {"must be less than %{number}",
+                   [validation: :number, kind: :less_than, number: 101]}
+              ]}
   end
 
   test "delete/1 valid" do
