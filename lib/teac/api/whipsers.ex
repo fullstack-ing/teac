f27fb47ed867567f453d2ce430b992ef7b480149
FullStack.ing <186414880+fullstack-ing@users.noreply.github.com>
2025-07-18 16:33:35 -0700

    feat: complete whisper.post/1, closes #136

--- lib/teac/api/whipsers.ex
+++ lib/teac/api/whipsers.ex
@@ -1,19 +1,51 @@
 defmodule Teac.Api.Whipsers do
-  def post(opts) do
-    token = Keyword.fetch!(opts, :token)
-    client_id = Keyword.get(opts, :client_id, Teac.client_id())
+  alias Teac.Api
+  import Ecto.Changeset
 
-    case Req.post!(Teac.api_uri() <> "whispers",
-           headers: [
-             {"Authorization", "Bearer #{token}"},
-             {"Client-Id", client_id},
-             {"Content-Type", "application/x-www-form-urlencoded"}
-           ],
-           form: [],
-           decode_body: :json
-         ) do
-      %Req.Response{status: 200, body: %{"data" => data}} -> {:ok, data}
-      %Req.Response{body: body} -> {:error, body}
+  def post(opts) do
+    case validate_get_opts(opts) do
+      {:ok,
+       %{
+         token: token,
+         client_id: client_id,
+         from_user_id: from_user_id,
+         to_user_id: to_user_id,
+         message: message
+       }} ->
+        [
+          base_url: Api.uri("whispers"),
+          params: [from_user_id: from_user_id, to_user_id: to_user_id],
+          headers: Api.headers(token, client_id),
+          json: %{message: message}
+        ]
+        |> Keyword.merge(Application.get_env(:teac, :api_req_options, []))
+        |> Req.post!()
+        |> Api.handle_response()
+
+      error ->
+        error
+    end
+  end
+
+  defp validate_get_opts(opt) do
+    types = %{
+      token: :string,
+      client_id: :string,
+      from_user_id: :string,
+      to_user_id: :string,
+      message: :string
+    }
+
+    changeset =
+      {%{}, types}
+      |> cast(Map.new(opt), Map.keys(types))
+      |> Api.default_client_id()
+      |> validate_required([:token, :from_user_id, :to_user_id, :message])
+      |> validate_length(:message, max: 10_000)
+
+    case apply_action(changeset, :insert) do
+      {:ok, data} -> {:ok, data}
+      {:error, %Ecto.Changeset{errors: errors}} -> {:error, errors}
     end
   end
 end
--- test/teac/api/whipsers_test.exs
+++ test/teac/api/whipsers_test.exs
@@ -1,12 +1,29 @@
 defmodule Teac.Api.WhipsersTest do
   use ExUnit.Case, async: true
+  alias Teac.Api.Whipsers
+
+  @args [
+    token: "token",
+    client_id: "asdf",
+    from_user_id: "asdf",
+    to_user_id: "asdf",
+    message: "asdf"
+  ]
 
   test "post/1 valid" do
     Req.Test.stub(Teac.Api, fn conn ->
       Req.Test.json(conn, %{"data" => %{}})
     end)
 
-    # fixme
-    assert false
+    assert Whipsers.post(@args) == {:ok, %{}}
+    assert Whipsers.post(@args |> Keyword.delete(:client_id)) == {:ok, %{}}
+  end
+
+  test "post/1 invalid" do
+    Req.Test.stub(Teac.Api, fn conn ->
+      Req.Test.json(conn, %{"data" => %{}})
+    end)
+
+    assert Whipsers.post(@args) == {:ok, %{}}
   end
 end
